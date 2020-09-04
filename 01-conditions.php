502332ff23925fdcde264f4770e07e3fc39ba0b0
servietskyCF2M <web2020.virgile@gmail.com>
2020-09-04 14:27:42 +0200

    Exo - Ternaire - Virgile

--- 01-conditions.php
+++ 01-conditions.php
@@ -41,18 +41,29 @@ if($hasard<5){
 }
 
 ?>
+<h2>Condition ternaire: on peut faire ce que fait un if / else en une ligne seulement</h2>
+<?php
+//Faites une ternaire sur hasard avec les résultats de l'exemple précédent mise dans une variable, la condition est que $hasard est ou plus petit que 5 ou plus grand que 5 ou égal à 5
+
+$ternaire = $hasard < 5 ? '$hasard vaut '."$hasard et est donc plus petit que 5" : '$hasard vaut '."$hasard et est donc plus grand que 5";
+echo $ternaire;
+?>
 <p>On peut imbriquer les conditions if else, en général on évite d'avoir plus de 3 niveaux d'imbrication</p>
 <h5>
     <?php
+    echo '<hr>$hasard vaut '.$hasard.' <br>$hasard/2 = '.($hasard/2).'<br> et donc le modulo de $hasard%2 est le nombre d\'éléments derrière la virgule vaut '.($hasard%2).'<hr>';
+
     // si hasard est plus petit que 5
     if($hasard<5){
 
         /*
          * remplacer la condition if($hasard){ pour vérifier si hasard est impaire
+         * Correction
          */
 
-        // si hasard est impaire
-        if($hasard%2 == 1){
+
+        // si hasard est impaire => $hasard%2 => reste de la division vaut autre chose que 0 (dans notre cas 1)
+        if($hasard%2){
             echo $hasard." est plus petit que 5 et est impaire";
             // si hasard est paire
         }else{
@@ -61,7 +72,7 @@ if($hasard<5){
 // si hasard est plus grand ou égal à 5
     }else{
         // si hasard est impaire
-        if($hasard%2 ==1){
+        if($hasard%2){
             echo $hasard." est plus grand ou égale à 5 et est impaire";
             // si hasard est paire
         }else{
@@ -70,5 +81,40 @@ if($hasard<5){
     }
     ?>
 </h5>
+<h1>condition sinon si: elseif ou else if</h1>
+<p>On verifie si 1 condition est remplie SINON SI (elseif) on vérifie une autre condition, SINON on effectue une action par défaut</p>
+<?php
+
+// SI on va vérifier si le chiffre au hasard est plus petit que 5 et qu'il est impaire
+if($hasard<5 && $hasard%2){
+    echo '$hasard vaut '."$hasard et est donc plus petit que 5 et est impaire";
+
+// SINON SI hasard est plus petit que 5 et qu'il est paire !() => inverse d'impaire
+}else if($hasard<5 && !($hasard%2) ){
+    echo '$hasard vaut '."$hasard et est donc plus petit que 5 et est paire";
+
+// SINON SI on va vérifier si le chiffre au hasard est plus grand ou égal à 5 et qu'il est impaire
+}elseif($hasard>=5 && $hasard%2){
+
+    echo '$hasard vaut '. "$hasard et est donc plus grand ou égal à 5 et est impaire";
+// SINON tout les cas de figures possibles on été listés précédemment, le else peut donc nous afficher la dernière option possible
+}else{
+    echo '$hasard vaut '. "$hasard et est donc plus grand ou égal à 5 et est paire";
+}
+
+?>
+
+
+<h3>Explication sur le modulo</h3>
+<p>le modulo d'un chiffre nous donne le nombre d'éléments derrière la virgule après la division de celui-ci</p>
+<hr>
+<?php
+$a = 8;
+$b = $a%2; // on divise par 8 et on regarde si il y a quelque chose après la virgule
+$c = $a%3; // on divise 8 par 3 et on regarde combien on a d'éléments dans le cas de 8 / 3, on peut 2x 3 dedans = 6, il faut rajouter 2 pour arriver à 8
+echo $b." | ".$c;
+echo "<br>".($a/3);
+?>
+<hr>
 </body>
 </html>
